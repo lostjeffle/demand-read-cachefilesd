395866d896543d25b4ab2f1d8a61620ff8a22d9e
Jeffle Xu <jefflexu@linux.alibaba.com>
2022-01-13 14:16:53 +0800

    cachefilesd2: use hashing algorithm
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>

--- cachefilesd2.c
+++ cachefilesd2.c
@@ -18,45 +18,35 @@ struct cachefiles_req_in {
         char path[NAME_MAX];
 };
 
-#define SRC_IMG_PATH "/root/Dtest.img"
-
-static char *get_path_hash(char *path)
-{
-	if (!strcmp(path, "Dtest.img"))
-		return "@9c";
-	else if (!strcmp(path, "Dblob1.img"))
-		return "@b5";
-	else
-		return NULL;
-}
+#define CACHEFILES_ROOT	"/root/"
+#define SRC_IMG_PATH "/root/"
 
 int process_one_req(int fd)
 {
-	int ret;
+	int ret, retval = -1;
 	struct cachefiles_req_in req_in;
 	int dst_fd, src_fd;
 	char dst_path[NAME_MAX];
 	char src_path[NAME_MAX];
 	off64_t src_off, dst_off;
 	char cmd[32];
-	char *buffer, *hash;
+	unsigned char fan;
+	char *buffer;
 	size_t len;
 
 	ret = read(fd, &req_in, sizeof(req_in));
-	if (ret <= 0) {
-		printf("read /dev/cachefiles failed\n");
+	if (ret < 0)
+		printf("read /dev/cachefiles_ondemand failed\n");
+	if (ret <= 0)
 		goto err;
-	}
-	printf("[%llu %s] off %llu, len %llu\n", req_in.id, req_in.path, req_in.off, req_in.len);
-	
-	hash = get_path_hash(req_in.path);
-	if (!hash) {
-		printf("backing file path parsing failed\n");
-		goto err;
-	}
 
-	snprintf(src_path, sizeof(src_path), "/root/%s", req_in.path);
-	snprintf(dst_path, sizeof(dst_path), "/root/cache/Ierofs/%s/%s", hash, req_in.path);
+	printf("[id %llu %s] off %llx, len %llx\n", req_in.id, req_in.path, req_in.off, req_in.len);
+	
+	/* strip the prefix 'D' of the path */
+	fan = get_cookie_fan(req_in.path + 1);
+
+	snprintf(src_path, sizeof(src_path), "%s/%s", SRC_IMG_PATH, req_in.path);
+	snprintf(dst_path, sizeof(dst_path), "%s/cache/Ierofs/@%2x/%s", CACHEFILES_ROOT, fan, req_in.path);
 
 	src_fd = open(src_path, O_RDWR);
 	if (src_fd < 0) {
@@ -89,24 +79,14 @@ int process_one_req(int fd)
 		goto err_buffer;
 	}
 
-	/*
- 	 * Writeback data to disk since fscache access backing file through
- 	 * DIRECT IO currently.
- 	 */
-	ret = fsync(dst_fd);
-	if (ret < 0) {
-		printf("fdatasync failed\n");
-		goto err_buffer;
-	}
-
 	snprintf(cmd, sizeof(cmd), "done %llu", req_in.id);
 	ret = write(fd, cmd, strlen(cmd));
 	if (ret < 0) {
 		printf("done failed\n");
-		return -1;
+		goto err_buffer;
 	}
 
-	return 0;
+	retval = 0;
 
 err_buffer:
 	free(buffer);
@@ -115,7 +95,7 @@ err_dstfd:
 err_srcfd:
 	close(src_fd);
 err:
-	return -1;
+	return retval;
 }
 
 
@@ -124,26 +104,19 @@ int main(void)
 {
 	int fd, ret;
 	char *cmd;
+	char cmdbuf[NAME_MAX];
 	struct pollfd pollfd;
 
-	fd = open("/dev/cachefiles_demand", O_RDWR);
+	fd = open("/dev/cachefiles_ondemand", O_RDWR);
 	if (fd < 0) {
 		printf("open failed\n");
 		return -1;
 	}
 
-	cmd = "dir /root";
-	ret = write(fd, cmd, strlen(cmd));
+	snprintf(cmdbuf, sizeof(cmdbuf), "dir %s", CACHEFILES_ROOT);
+	ret = write(fd, cmdbuf, strlen(cmdbuf));
 	if (ret < 0) {
 		printf("write dir failed, %d\n", errno);
-		close(fd);
-		return -1;
-	}
-
-	cmd = "mode demand";
-	ret = write(fd, cmd, strlen(cmd));
-	if (ret < 0) {
-		printf("write mode failed, %d\n", errno);
 		close(fd);
 		return -1;
 	}
@@ -177,13 +150,11 @@ int main(void)
 	
 		if (ret == 0 || !(pollfd.revents & POLLIN)) {
 			printf("poll returned %d (%x)\n", ret, pollfd.revents);
-			sleep(1);
 			continue;
 		}
 
-		ret = process_one_req(fd);
-		if (ret < 0)
-			sleep(1);
+		/* process all pending read requests */
+		while (!process_one_req(fd)) {}
 	}
 
 	return 0;
