71d20b26a440e9cfdfa43323df40149780647029
Jeffle Xu <jefflexu@linux.alibaba.com>
2022-06-24 17:06:10 +0800

    refactor test14
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>

--- test/test14.sh
+++ test/test14.sh
@@ -12,18 +12,7 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
-_volume1="erofs,$_bootstrap1"
-volume1="I$_volume1"
-bootstrap_fan1=$(../getfan $_volume1 $_bootstrap1)
-bootstrap_path1="$fscachedir/cache/$volume1/@$bootstrap_fan1/D$_bootstrap1"
-
-_volume2="erofs,$_bootstrap2"
-volume2="I$_volume2"
-bootstrap_fan2=$(../getfan $_volume2 $_bootstrap2)
-bootstrap_path2="$fscachedir/cache/$volume2/@$bootstrap_fan2/D$_bootstrap2"
-
-rm -f $bootstrap_path1
-rm -f $bootstrap_path2
+rm -rf "$fscachedir/cache/"
 cp -f img/noinline/test.img ./$_bootstrap1
 cp -f img/noinline/test.img ./$_bootstrap2
 
