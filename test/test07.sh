9cb976d90766b7f34891da2d31dbe5d599673e8d
Jeffle Xu <jefflexu@linux.alibaba.com>
2022-06-24 16:15:04 +0800

    refactor test07
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>

--- test/test07.sh
+++ test/test07.sh
@@ -8,7 +8,6 @@
 
 
 fscachedir="/root"
-_bootstrap="test.img"
 
 make > /dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -16,15 +15,8 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
-_volume="erofs,$_bootstrap"
-volume="I$_volume"
-
-bootstrap_fan=$(../getfan $_volume $_bootstrap)
-bootstrap_path="$fscachedir/cache/$volume/@$bootstrap_fan/D$_bootstrap"
-
-
 # make cache file ready under root cache directory
-rm -f $bootstrap_path
+rm -rf "$fscachedir/cache/"
 cp img/noinline/test.img .
 
 bash -c "./test05-daemon $fscachedir 5 > /dev/null  &"
